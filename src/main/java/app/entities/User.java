ff3e67c3fdf9ec4b47e6ea5ffa73748d3702e416
Dima <nevar.dima@gmail.com>
2019-09-29 12:31:30 +0300

    rentcars app branch1

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -1,17 +1,19 @@
 package app.entities;
 
-import java.util.Objects;
-
 public class User {
     private String name;
     private String password;
+    private String phone;
+    private String email;
 
     public User(){
 
     }
-    public User(String name, String password){
+    public User(String name, String password, String phone, String email){
         this.name = name;
         this.password = password;
+        this.phone = phone;
+        this.email = email;
     }
 
     public String getName() {
@@ -28,6 +30,22 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+
+    public void setPhone (String phone){
+        this.phone = phone;
+    }
+
+    public String getEmail (){
+        return email;
+    }
+
+    public void setEmail (String email){
+        this.email = email;
     }
 
     @Override
@@ -54,6 +72,8 @@ public class User {
         return "User{" +
                 "name='" + name + '\'' +
                 ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
                 '}';
     }
 }
